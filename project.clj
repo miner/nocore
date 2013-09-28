ef4190ad4a50da47b54018dc3ee3837d5e141c71
Steve Miner <steveminer@gmail.com>
2013-09-28 11:15:30 -0400

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nocore/lein-template "0.1.0-SNAPSHOT"
+(defproject nocore/lein-template "0.1.1"
   :description "Leiningen templat for a nocore Clojure project"
   :url "https://github.com/miner/nocore"
   :license {:name "Eclipse Public License"
