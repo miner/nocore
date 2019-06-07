ef9ba8a7e6e06a54820b6fa56db88ab0806ee41a
Steve Miner <steveminer@gmail.com>
2019-06-07 09:46:55 -0400

    updating Clojure version

--- src/leiningen/new/nocore/project.clj
+++ src/leiningen/new/nocore/project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/{{user}}/{{name}}"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]])
+
 
